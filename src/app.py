257f55bb17e3ba8981b688cdd1921d82d14f6b44
Federico Resano <123256603+fede1525@users.noreply.github.com>
2024-02-03 02:00:21 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -12,31 +12,27 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_family_members():
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/members/<int>:id', methods=['GET'])
+def get_specific_member():
+    member = jackson_family.get_member()
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
